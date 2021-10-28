2a2c15fc0cd8ee58fa360a9680c5185d9a72c1d1
Lionnel Tsuro <55039869+LionnelT@users.noreply.github.com>
2021-10-28 12:42:10 +0200

    added files

--- app/src/main/java/com/example/notesapplication/MainActivity.kt
+++ app/src/main/java/com/example/notesapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.notesapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var notesRV: RecyclerView
+    lateinit var addFAB: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notesRV = findViewById(R.id.idRVNotes)
+        addFAB = findViewById(R.id.idFABAddNote)
     }
 }
